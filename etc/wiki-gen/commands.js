3cfa3c0ec3920d91f570f9268c74c7db21967461
AlcaDesign <jfost3@gmail.com>
2016-04-01 10:04:30 -0500

    Formatting some commands

--- etc/wiki-gen/commands.js
+++ etc/wiki-gen/commands.js
@@ -1,15 +1,15 @@
-Copy `commands` from ./lib/corecommand.js and then run:
+#### `########` #### ./###/###########.## ### #### ###:
 
-commands
-	.filter(n =>
-		n.action === null && n.permissionLevel !== 'bot'
+########
+	.######(# =>
+		#.###### === #### && #.############### !== '###'
 	)
-	.map(n => [
-				'##' + n.name,
-				'Aliases: ' + n.alias.join(', '),
-				'Permission: ' + ((n.permissionLevel === true ? 'everyone' : n.permissionLevel)
-					.replace(/Up/, ' and up'))
+	.###(# => [
+				'##' + #.####,
+				'#######: ' + #.#####.####(', '),
+				'##########: ' + ((#.############### === #### ? '########' : #.###############)
+					.#######(/##/, ' ### ##'))
 			]
-		.join('\n\r') + '\n\r'
+		.####('\#\#') + '\#\#'
 	)
-	.join('\n\r')
+	.####('\#\#')
